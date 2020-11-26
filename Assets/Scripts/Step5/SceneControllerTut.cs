6f2c52393b56139265cfb9dc0780af6121ccc60f
eddiman <edbjorgen@gmail.com>
2020-11-26 18:11:30 +0100

    cleared tut script

--- Assets/Scripts/Step5/SceneControllerTut.cs
+++ Assets/Scripts/Step5/SceneControllerTut.cs
@@ -7,34 +7,5 @@ using UnityEngine.SceneManagement;
 
 public class SceneControllerTut : MonoBehaviour
 {
-    public UnityEvent EnemiesDefeated;
-
-    public TextMeshProUGUI enemyText;
-    // Start is called before the first frame update
-    [SerializeField]
-    private int _allEnemiesCount;
-    [SerializeField]
-    private int _deadEnemiesCount;
-
-    void Start()
-    {
-        _allEnemiesCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        enemyText.text = "Boxes left: " + _deadEnemiesCount + "/" + _allEnemiesCount;
-    }
-
-    public void checkIfAllEnemiesAreDead()
-    {
-        _deadEnemiesCount++;
-        if (_deadEnemiesCount == _allEnemiesCount)
-        {
-            EnemiesDefeated.Invoke();
-        }
-        enemyText.text = "Boxes left: " + _deadEnemiesCount + "/" + _allEnemiesCount;
-    }
-
-        public void SceneReload()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
-    }
 
 }
